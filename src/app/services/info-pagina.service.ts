e4bbced0c20ec187ba5c51f8f0655122dd3c5781
Esteban <juanesbs2003@hotmail.com>
2023-06-08 12:13:13 -0500

    añadiendo JSON incompletos

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  constructor() {
+    console.log("servicio pagina cargada");
+
+    //leer el archivo JSON
+
+   }
+}
