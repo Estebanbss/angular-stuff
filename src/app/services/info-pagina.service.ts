76319ceedd6b131c5dff4e560fa7f518e4f75e7e
Esteban <juanesbs2003@hotmail.com>
2023-06-08 17:56:03 -0500

    Añadiendo json a la pagina para los datos de la pagina

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,14 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  constructor() {
+  info: InfoPagina = {};
+  cargada = false;
+
+  constructor( private http: HttpClient) {
     console.log("servicio pagina cargada");
 
     //leer el archivo JSON
+    this.http.get("assets/data/data-pagina.json")
+        .subscribe( (resp: InfoPagina) =>{
+            this.cargada = true;
+            this.info = resp;
+            console.log(resp);
+        }
 
+          );
    }
 }
