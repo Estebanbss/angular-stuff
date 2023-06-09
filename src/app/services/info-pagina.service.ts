f0ffb4bbbd714fb2d79f044d8d286966b9e36b30
Esteban <juanesbs2003@hotmail.com>
2023-06-09 13:27:45 -0500

    añadiendo servicios get con firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -5,22 +5,38 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 @Injectable({
   providedIn: 'root'
 })
+
 export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
     console.log("servicio pagina cargada");
 
-    //leer el archivo JSON
+    this.cargarInfo();
+    this.cargarEquipo();
+   }
+
+   private cargarInfo(){
     this.http.get("assets/data/data-pagina.json")
         .subscribe( (resp: InfoPagina) =>{
             this.cargada = true;
             this.info = resp;
-            console.log(resp);
+            
         }
 
           );
    }
+
+   private cargarEquipo(){
+    this.http.get("https://angular-html-2fec1-default-rtdb.firebaseio.com/Equipo.json")
+        .subscribe( (resp:any) => {
+          this.equipo = resp;
+          console.log(resp);
+        });
+
+   }
 }
+
